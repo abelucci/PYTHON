7544c8336f4c17a799325a021335501667f835cd
abelucci <abelash.delsur@gmail.com>
2025-08-07 14:56:48 -0300

    add files

---
+++ CLASE1/desafío2v2.py
@@ -0,0 +1,7 @@
+def mostrar_edad(nombre, edad): #agrego los parámetros que quiero trabajar, algo dinámico
+    print("Hola " + nombre + ", tenés " + str(edad) + " años.")
+    # Usando concatenación (+) completa, se requiere conversión de números a texto (string)
+
+mostrar_edad("Lucas", 21)
+mostrar_edad("Valentina", 18) 
+
