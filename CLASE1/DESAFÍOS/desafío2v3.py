4d2caa7c73ef556538e1b03ea6bab80a5d5b613a
abelucci <abelash.delsur@gmail.com>
2025-08-11 17:32:18 -0300

    change

--- CLASE1/DESAFÍOS/desafío2v3.py
+++ CLASE1/DESAFÍOS/desafío2v3.py
@@ -1,10 +1,5 @@
-def mostrar_edad(nombre, edad): #agrego los parámetros que quiero trabajar, algo dinámico
+def mostrar_edad(nombre, edad): 
     print(f"Hola, {nombre}, tenés, {edad}, años.")
-    # usando f-string.
-    # Dentro de las llaves {} va cualquier variable o expresión, 
-    # y todo está "contenido" en un único string que empieza con f"...".
-    # Una f-string (o formatted string) es una forma moderna y eficiente de incluir variables dentro de un texto en Python. 
-    # Se introdujo en Python 3.6 y usa una letra f antes del string.
-
+    
 mostrar_edad("Lucas", 21)
 mostrar_edad("Valentina", 18) 
